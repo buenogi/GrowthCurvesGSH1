aa816f08c94e2aa5d11f0611bc1e9e9c985b237e
bueno.gi <gislayne.bueno@gmail.com>
2023-08-02 00:00:48 -0300

    Saving EDA script

--- Scripts/02_EDA.R
+++ Scripts/02_EDA.R
@@ -9,6 +9,12 @@ library(ggplot2)
 #Loading data
 DataGC <- read.csv(file = "Data/Processed/DataGC.csv")
 
+# Checking classes
+
+sapply(DataGC, class)
+
+DataGC$experiment <- as.factor(DataGC$experiment)
+DataGC$conc <- as.factor(DataGC$conc)
 # EDA
 
 DataGC$tempo <- as.factor(DataGC$tempo)
@@ -19,57 +25,49 @@ DataGC_SUM_exp <- DataGC%>%
 
 # Box-plot
 
-GrowthC_boxplot <- ggplot(DataGC_SUM_exp, aes(tempo, log(abs_exp), 
+GrowthC_DotPlot <- ggplot(DataGC_SUM_exp, aes(tempo, abs_exp, 
                                               shape = conc, 
                                               color = experiment)) +
   geom_point()+
-  ggtitle("Promastigotes growth with and without 150μM of SbIII") +
+  ggtitle("GSH1 Hemi mutants promastigotes growth with and without 150μM of SbIII") +
   labs(x = " Time (h)  ", y = "Abs")+
   theme(plot.title = element_text(size = 14,face="bold"),
         axis.text.x = element_text(size = 10), 
         axis.text.y = element_text(size = 10),
         axis.title.x = element_text(size = 15),
         axis.title.y = element_text(size = 15))+
-  facet_wrap(DataGC_SUM_exp$pop, nrow = 2)+
+  facet_wrap(~pop, nrow = 2)+
   theme_bw()
 
-GrowthC_boxplot
+GrowthC_DotPlot
 
-
-
-#Data check
-
-sapply(DataGC, class)
-DataGC$pop <- as.factor(DataGC$pop)
-DataGC$tempo <- as.numeric(DataGC$tempo)
-DataGC$conc <- as.factor(DataGC$conc)
-DataGC$experiment <- as.factor(DataGC$experiment)
-sapply(DataGC, class)
-
+ggsave("Figuras/01_GrowthC_DotPlot.png")
 # Sumarizing
 
 DataGC_sum <- DataGC%>%
   group_by(pop, conc, tempo)%>%
   summarise(mean_value = mean(abs), sd_value = sd(abs))
 
-GrowthC_plot <- ggplot(DataGC_sum, aes(tempo, log(mean_value), group = conc)) +
+GrowthC_plot_Clone <- ggplot(DataGC_sum, aes(tempo, log(mean_value), group = conc)) +
   geom_line(aes(color = conc))+
-  ggtitle("Promastigotes growth with and without 150μM of SbIII") +
+  ggtitle("GSH1 Hemi mutants promastigotes growth with and without 150μM of SbIII") +
   labs(x = " Time (h)  ", y = "Abs")+
   theme(plot.title = element_text(size = 14,face="bold"),
         axis.text.x = element_text(size = 10), 
         axis.text.y = element_text(size = 10),
         axis.title.x = element_text(size = 15),
         axis.title.y = element_text(size = 15))+
-  facet_wrap(DataGC_sum$pop, nrow = 2)+
+  scale_x_discrete(labels = c("0h", "24h", "48h", "72h", "96h")) +
+  facet_wrap(~pop, nrow = 2)+
   theme_bw()
 
-GrowthC_plot
+GrowthC_plot_Clone
+ggsave("Figuras/02_GrowthC_plot_Clone.png")
 
 # Todas juntas
 
 
-GrowthC_plot_2 <- ggplot(DataGC_sum, aes(tempo, mean_value, group = pop)) +
+GrowthC_plot <- ggplot(DataGC_sum, aes(tempo, mean_value, group = pop)) +
   geom_line(aes(color  = pop), size = 0.5)+
   ggtitle("Promastigotes growth with and without 150μM of SbIII") +
   labs(x = " Time (h)  ", y = "Abs")+
@@ -78,7 +76,10 @@ GrowthC_plot_2 <- ggplot(DataGC_sum, aes(tempo, mean_value, group = pop)) +
         axis.text.y = element_text(size = 10),
         axis.title.x = element_text(size = 15),
         axis.title.y = element_text(size = 15))+
+  scale_x_discrete(labels = c("0h", "24h", "48h", "72h", "96h")) +
   facet_grid(DataGC_sum$conc)+
   theme_bw()
 
-GrowthC_plot_2 + labs(color = "Populations")
+GrowthC_plot + labs(color = "Populations")
+ggsave("Figuras/03_GrowthC_plot.png")
+
